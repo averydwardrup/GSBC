60be8a75663af00bbde4c28f30261777b0fafd49
averydwardrup <averydwardrup@users.noreply.github.com>
2018-10-17 23:58:29 -0400

    new themes

---
+++ themes/flat-theme/header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php wp_title( '|', true, 'right' ); ?></title>
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri() ?>/assets/js/html5shiv.js"></script>
+<script src="<?php echo get_template_directory_uri() ?>/assets/js/respond.min.js"></script>
+<![endif]-->       
+<?php zee_favicon();?>
+<?php wp_head(); ?>
+<!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous"> -->
+<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "2544edd7-9503-496a-8483-315dae4f520f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+
+</head><!--/head-->
+
+<body <?php body_class() ?>>
+  <?php if(zee_option('zee_theme_layout')=='boxed'){ ?>
+    <div id="boxed">
+  <?php } ?>
+
+  <header id="header" class="navbar navbar-inverse navbar-fixed-top" role="banner">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only"><?php _e('Toggle navigation', ZEETEXTDOMAIN); ?></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <?php logo();?>
+      </div>
+
+      <div class="hidden-xs">
+        <?php 
+        if ( has_nav_menu( 'primary' ) ) {
+          wp_nav_menu( array(
+            'theme_location'  => 'primary',
+            'container'       => false,
+            'menu_class'      => 'nav navbar-nav navbar-main',
+            'fallback_cb'     => 'wp_page_menu',
+            'walker'          => new wp_bootstrap_navwalker()
+            )
+          ); 
+        }
+        ?>
+      </div>
+
+      <div id="mobile-menu" class="visible-xs">
+        <div class="collapse navbar-collapse">
+          <?php 
+          if ( has_nav_menu( 'primary' ) ) {
+            wp_nav_menu( array(
+              'theme_location'  => 'primary',
+              'container'       => false,
+              'menu_class'      => 'nav navbar-nav',
+              'fallback_cb'     => 'wp_page_menu',
+              'walker'          => new wp_bootstrap_mobile_navwalker()
+              )
+            ); 
+          }
+          ?>
+        </div>
+      </div><!--/.visible-xs-->
+    </div>
+  </header><!--/#header-->
+
+  <?php get_template_part( 'sub', 'title' ); ?>
+
+  <?php if( ! is_page() ) { ?>
+  <section id="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div id="primary" class="content-area">
+            <?php } ?>
+
